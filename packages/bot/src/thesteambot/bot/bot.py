c06ae0541d81ac6d977e8f39149aecb047b38d0f
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-15 19:54:44 -0400

    feat: enable members intent in discord bot

--- packages/bot/src/thesteambot/bot/bot.py
+++ packages/bot/src/thesteambot/bot/bot.py
@@ -4,7 +4,10 @@ from discord.ext import commands
 
 class Bot(commands.Bot):
     def __init__(self) -> None:
+        intents = discord.Intents.default()
+        intents.members = True
+
         super().__init__(
             command_prefix="xkcd",
-            intents=discord.Intents.default(),
+            intents=intents,
         )
