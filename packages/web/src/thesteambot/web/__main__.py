9d5c7a0149055080d2a2e1a27d6bfed338820728
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-15 20:16:03 -0400

    feat: set up bare minimum webserver

--- packages/web/src/thesteambot/web/__main__.py
+++ packages/web/src/thesteambot/web/__main__.py
@@ -1,5 +1,24 @@
+from starlette.applications import Starlette
+from starlette.requests import Request
+from starlette.responses import PlainTextResponse, Response
+from starlette.routing import Route
+
+
+def homepage(request: Request) -> Response:
+    return PlainTextResponse("Hello world!")
+
+
+app = Starlette(
+    routes=[
+        Route("/", homepage),
+    ],
+)
+
+
 def main() -> None:
-    print(f"Hello from {__package__}!")
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0")
 
 
 if __name__ == "__main__":
