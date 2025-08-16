be6cf2c61ba6ddeb81fae2384c3ecb6c6227dcfe
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-15 21:19:00 -0400

    feat(web): add typical middleware

--- packages/web/src/thesteambot/web/__main__.py
+++ packages/web/src/thesteambot/web/__main__.py
@@ -1,7 +1,17 @@
 from starlette.applications import Starlette
+from starlette.config import Config
+from starlette.datastructures import Secret
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.middleware.trustedhost import TrustedHostMiddleware
 from starlette.requests import Request
 from starlette.responses import PlainTextResponse, Response
 from starlette.routing import Route
+
+config = Config()
+DOMAIN = config("DOMAIN")
+WEB_SECRET_KEY = config("WEB_SECRET_KEY", cast=Secret)
 
 
 def homepage(request: Request) -> Response:
@@ -11,6 +21,19 @@ def homepage(request: Request) -> Response:
 app = Starlette(
     routes=[
         Route("/", homepage),
+    ],
+    middleware=[
+        Middleware(CORSMiddleware),
+        Middleware(SessionMiddleware, secret_key=WEB_SECRET_KEY, https_only=True),
+        Middleware(
+            TrustedHostMiddleware,
+            allowed_hosts=[
+                DOMAIN,
+                "localhost",
+                "127.0.0.1",
+            ],
+            www_redirect=False,
+        ),
     ],
 )
 
