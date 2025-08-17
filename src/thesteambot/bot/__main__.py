f1028a514cfa5023d16da94de5103bfdcb9ca50c
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-16 21:51:44 -0400

    fix: use bot.start() to not re-initialize event loop

--- src/thesteambot/bot/__main__.py
+++ src/thesteambot/bot/__main__.py
@@ -1,6 +1,8 @@
 import asyncio
 import os
 
+import discord
+
 from thesteambot.bot.bot import Bot
 from thesteambot.db.connection import create_pool
 
@@ -13,9 +15,11 @@ async def async_main() -> None:
     extensions = os.environ["BOT_EXTENSIONS"].split(",")
     token = os.environ["BOT_TOKEN"]
 
+    discord.utils.setup_logging(root=True)
+
     async with create_pool() as pool:
         bot = Bot(extensions=extensions, pool=pool)
-        bot.run(token, root_logger=True)
+        await bot.start(token)
 
 
 if __name__ == "__main__":
