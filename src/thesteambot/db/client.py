11548b01cd11f9e9cf3f3d71fb516ff78c26a9e4
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-17 19:29:58 -0400

    refactor(db): extract add_discord_user() method

--- src/thesteambot/db/client.py
+++ src/thesteambot/db/client.py
@@ -6,6 +6,12 @@ class DatabaseClient:
     def __init__(self, conn: asyncpg.Connection) -> None:
         self.conn = conn
 
+    async def add_discord_user(self, user_id: int) -> None:
+        await self.conn.execute(
+            "INSERT INTO discord_user (user_id) VALUES ($1) ON CONFLICT DO NOTHING",
+            user_id,
+        )
+
     async def get_discord_oauth(self, user_id: int) -> asyncpg.Record | None:
         return await self.conn.fetchrow(
             "SELECT * FROM discord_oauth WHERE user_id = $1",
@@ -27,10 +33,7 @@ class DatabaseClient:
             expires_in = datetime.timedelta(seconds=expires_in)
         expires_at = now + expires_in
 
-        await self.conn.execute(
-            "INSERT INTO discord_user (user_id) VALUES ($1) ON CONFLICT DO NOTHING",
-            user_id,
-        )
+        await self.add_discord_user(user_id)
         await self.conn.execute(
             "INSERT INTO discord_oauth "
             "(user_id, access_token, token_type, expires_at, refresh_token, scope) "
