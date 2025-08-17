5e0f8d412b703f5282e4453b175e4b0cfc402562
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-16 21:29:14 -0400

    feat: persist oauth handshake and user IDs

--- src/thesteambot/web/auth.py
+++ src/thesteambot/web/auth.py
@@ -1,10 +1,21 @@
 from authlib.integrations.base_client import OAuthError
+from pydantic import BaseModel
 from starlette.requests import Request
 from starlette.responses import Response
 from starlette.routing import Route
 
+from thesteambot.db.client import DatabaseClient
+from thesteambot.web.lifespan import cast_state
 from thesteambot.web.oauth import oauth
 from thesteambot.web.templating import templates
+
+
+class DiscordAccessToken(BaseModel):
+    access_token: str
+    token_type: str
+    expires_in: float
+    refresh_token: str
+    scope: str
 
 
 async def authorize_discord(request: Request) -> Response:
@@ -16,12 +27,27 @@ async def authorize_discord(request: Request) -> Response:
 
     try:
         token = await discord.authorize_access_token(request)
+        token = DiscordAccessToken.model_validate(token)
     except OAuthError:
         return templates.TemplateResponse(request, page, context=context)
 
-    # TODO: store access token
-    print(repr(token))
+    state = cast_state(request)
+    client = state.hikari_rest.acquire(token.access_token, token.token_type)
+    async with client:
+        user = await client.fetch_my_user()
 
+    async with state.pool.acquire() as conn:
+        client = DatabaseClient(conn)
+        await client.set_discord_oauth(
+            user.id,
+            access_token=token.access_token,
+            token_type=token.token_type,
+            expires_in=token.expires_in,
+            refresh_token=token.refresh_token,
+            scope=token.scope,
+        )
+
+    request.session["discord-user-id"] = user.id
     context["success"] = True
     return templates.TemplateResponse(request, page, context=context)
 
--- src/thesteambot/web/login.py
+++ src/thesteambot/web/login.py
@@ -1,25 +1,41 @@
 from starlette.requests import Request
-from starlette.responses import Response
+from starlette.responses import RedirectResponse, Response
 from starlette.routing import Route
 
+from thesteambot.db import DatabaseClient
+from thesteambot.web.lifespan import cast_state
 from thesteambot.web.oauth import oauth
 
 
 async def login_discord(request: Request) -> Response:
-    discord = oauth.create_client("discord")
-    assert discord is not None
+    async def authorize_redirect() -> Response:
+        discord = oauth.create_client("discord")
+        assert discord is not None
 
-    # TODO: check if user is already authorized
+        redirect_uri = request.url_for("auth", path="/discord")
+        scopes = ("connections", "identify")
 
-    redirect_uri = request.url_for("auth", path="/discord")
-    scopes = ("connections", "identify")
+        return await discord.authorize_redirect(
+            request,
+            redirect_uri,
+            scopes=" ".join(scopes),
+            prompt="none",
+        )
 
-    return await discord.authorize_redirect(
-        request,
-        redirect_uri,
-        scopes=" ".join(scopes),
-        prompt="none",
-    )
+    user_id = request.session.get("discord-user-id")
+    if user_id is None:
+        return await authorize_redirect()
+
+    state = cast_state(request)
+    async with state.pool.acquire() as conn:
+        client = DatabaseClient(conn)
+        row = await client.get_discord_oauth(user_id)
+
+    if row is None:
+        return await authorize_redirect()
+
+    # FIXME: show page or toast indicating they're already authenticated
+    return RedirectResponse(request.url_for("homepage"))
 
 
 routes = [
