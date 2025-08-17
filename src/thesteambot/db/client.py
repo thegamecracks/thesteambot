aea3a2e8f54ff80937f3ae431a121012ef2846c9
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-16 20:49:01 -0400

    feat: add discord tables and database client

---
+++ src/thesteambot/db/client.py
@@ -0,0 +1,44 @@
+import asyncpg
+
+
+class DatabaseClient:
+    def __init__(self, conn: asyncpg.Connection) -> None:
+        self.conn = conn
+
+    async def get_discord_oauth(self, user_id: int) -> asyncpg.Record | None:
+        return await self.conn.fetchrow(
+            "SELECT * FROM discord_oauth WHERE user_id = $1",
+            user_id,
+        )
+
+    async def set_discord_oauth(
+        self,
+        user_id: int,
+        *,
+        access_token: str,
+        token_type: str,
+        expires_in: float,
+        refresh_token: str,
+        scope: str,
+    ) -> None:
+        await self.conn.execute(
+            "INSERT INTO discord_user (user_id) VALUES ($1) ON CONFLICT DO NOTHING",
+            user_id,
+        )
+        await self.conn.execute(
+            "INSERT INTO discord_oauth "
+            "(user_id, access_token, token_type, expires_in, refresh_token, scope) "
+            "VALUES ($1, $2, $3, $4, $5) "
+            "ON CONFLICT (user_id) DO UPDATE SET "
+            "access_token = EXCLUDED.access_token, "
+            "token_type = EXCLUDED.token_type, "
+            "expires_in = EXCLUDED.expires_in, "
+            "refresh_token = EXCLUDED.refresh_token, "
+            "scope = EXCLUDED.scope",
+            user_id,
+            access_token,
+            token_type,
+            expires_in,
+            refresh_token,
+            scope,
+        )
---
+++ src/thesteambot/db/migrations/001-discord-oauth.sql
@@ -0,0 +1,13 @@
+CREATE TABLE discord_user (
+    user_id BIGINT PRIMARY KEY
+);
+
+-- https://discord.com/developers/docs/topics/oauth2
+CREATE TABLE discord_oauth (
+    user_id BIGINT PRIMARY KEY REFERENCES discord_user (user_id),
+    access_token TEXT NOT NULL,
+    token_type TEXT NOT NULL,
+    expires_in NUMERIC NOT NULL,
+    refresh_token TEXT NOT NULL,
+    scope TEXT NOT NULL
+);
