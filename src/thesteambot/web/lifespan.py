d97cf44ca328a114bc088bd8b91b2733741cdc71
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-16 21:23:09 -0400

    feat: add hikari to lifespan

--- src/thesteambot/web/lifespan.py
+++ src/thesteambot/web/lifespan.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from typing import Any, AsyncIterator, cast
 
 import asyncpg
+import hikari
 from starlette.applications import Starlette
 from starlette.requests import Request
 
@@ -9,13 +10,22 @@ from thesteambot.db import create_pool
 
 
 class State:
+    hikari_rest: hikari.RESTApp
     pool: asyncpg.Pool
 
 
 @asynccontextmanager
 async def lifespan(app: Starlette) -> AsyncIterator[dict[str, Any]]:
+    hikari_rest = hikari.RESTApp()
+    await hikari_rest.start()
+
     async with create_pool() as pool:
-        yield {"pool": pool}
+        yield {
+            "hikari_rest": hikari_rest,
+            "pool": pool,
+        }
+
+    await hikari_rest.close()
 
 
 def cast_state(request: Request) -> State:
