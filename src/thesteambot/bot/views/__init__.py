251085ef077a07b2e53ac8ebd2cada22a5dc7126
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-08-21 11:34:12 -0400

    refactor(bot): move steam views from oauth cog

--- src/thesteambot/bot/views/__init__.py
+++ src/thesteambot/bot/views/__init__.py
@@ -1,2 +1,13 @@
 from .base import CancellableView, View
-from .oauth import DiscordAuthorizeActionRow, create_authorize_view
+from .oauth import (
+    DiscordAuthorizeActionRow,
+    DiscordDeauthorizeActionRow,
+    DiscordDeauthorizeView,
+    ManageSteamUserSelect,
+    ManageSteamUserView,
+    SteamConnection,
+    SteamUserActionRow,
+    create_authorize_view,
+    create_manage_steam_user_view,
+    get_steam_ids,
+)
